05f3ea01622ec42dc719042cfbabc67f4f80b4f8
Jacob Taunton <jandrewt82@gmail.com>
2023-03-27 23:00:36 -0700

    test cases

---
+++ test_cases.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+from start_chat import run_single_test
+
+def run_test_cases():
+    test_cases = [
+        {
+            "command": "file:color.txt",
+            "question": "What is the color of the car according to color.txt file?"
+        },
+        {
+            "command": "curl:https://raw.githubusercontent.com/madtank/code/master/jokes/README.md",
+            "question": "In the README file you fetched, what type of database is used with the jokes application?"
+        },
+        {
+            "command": "curl:https://jsonplaceholder.typicode.com/todos/1",
+            "question": "What is the title of the first todo item?"
+        }
+    ]
+
+    for idx, test_case in enumerate(test_cases, 1):
+        print(f"Running test case {idx}...")
+        run_single_test(test_case['command'], test_case['question'])
+
+if __name__ == "__main__":
+    run_test_cases()
